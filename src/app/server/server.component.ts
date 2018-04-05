a717b8fc219be5a2d4decb01f7bd374585f00767
Ryan Sherrill <ryanbsherrill@gmail.com>
2018-04-04 23:10:12 -0400

    styling elements dynamically w/ ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { race } from "q";
 
 @Component({
   selector: 'app-server',
@@ -7,8 +8,16 @@ import { Component } from "@angular/core";
 export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
+  
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
